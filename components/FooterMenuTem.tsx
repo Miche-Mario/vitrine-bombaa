5a84503d567951d7331fca32c41f7c0b36d42bde
Miche-Mairo <gmespace.74@gmail.com>
2024-09-30 10:50:55 +0000

    fix build

--- components/FooterMenuTem.tsx
+++ components/FooterMenuTem.tsx
@@ -1,30 +1,35 @@
-import { data } from 'autoprefixer'
-import Link from 'next/link';
-import React from 'react'
+import { data } from "autoprefixer";
+import Link from "next/link";
+import React from "react";
 interface data {
-    title: string;
-    links: {
-        label: string;
-        link: string;
-    }[]
-  
+  title: string;
+  links: {
+    label: string;
+    link: string;
+  }[];
 }
 
 interface SectionProps {
-    dataa: data;
+  dataa: data;
 }
 
 const FooterMenuTem: React.FC<SectionProps> = ({ dataa }) => {
   return (
-    <div className='pt-4 md:pt-0'>
-        <p className='  text-lg font-poppinsBold'>{dataa.title}</p>
-        <div className=' flex flex-col  items-start md:mt-4 mt-2'>
-            {dataa.links.map((link, index) => (
-                <Link className='py-1 font-poppinsRegular text-md text-gray-700' href={link.link}>{link.label}</Link>
-            ))}
-        </div>
+    <div className="pt-4 md:pt-0">
+      <p className="  text-lg font-poppinsBold">{dataa.title}</p>
+      <div className=" flex flex-col  items-start md:mt-4 mt-2">
+        {dataa.links.map((link, index) => (
+          <Link
+            key={index}
+            className="py-1 font-poppinsRegular text-md text-gray-700"
+            href={link.link}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterMenuTem
+export default FooterMenuTem;
--- components/Section4.tsx
+++ components/Section4.tsx
@@ -1,57 +1,61 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import Emoji4 from '../public/image/Emoji4'
-import Vector4 from '../public/image/Vector4'
-import BlogCard from './BlogCard';
-import Button from './Button';
+import React, { useState } from "react";
+import Emoji4 from "../public/image/Emoji4";
+import Vector4 from "../public/image/Vector4";
+import BlogCard from "./BlogCard";
+import Button from "./Button";
 
 const Section4 = () => {
-    interface data {
-        mainTitle: string;
-        content: string
-    }
+  interface data {
+    mainTitle: string;
+    content: string;
+  }
 
-    const data: data = {
-        mainTitle: 'Pour mettre un article en vente',
-        content: 'Permettre aux vendeurs d\'ajouter le ou les localités ou ils peuvent livrer un frais fixe de livraison'
-    }
+  const data: data = {
+    mainTitle: "Pour mettre un article en vente",
+    content:
+      "Permettre aux vendeurs d&apos;ajouter le ou les localités ou ils peuvent livrer un frais fixe de livraison",
+  };
 
-    const [isGrowing, setIsGrowing] = useState(false);
-    const [isSliding, setIsSliding] = useState(false);
+  const [isGrowing, setIsGrowing] = useState(false);
+  const [isSliding, setIsSliding] = useState(false);
 
-
-    return (
-        <div>
-            <div className={` relative bg-lightprimary flex flex-col justify-center items-center  mx-4   sm:max-sm:pb-8 md:mx-24 mt-8 rounded-3xl md:py-20  px-[30px] py-20 mb-16 `}>
-
-                <div className=" rounded-lg overflow-hidden absolute hidden md:block  top-0 right-0">
-                    <Vector4 />
-                </div>
-
-
-                <div>
-                    <p className=' font-octarine text-center text-2xl md:text-4xl text-black'>
-                        Nous vous donnons nos conseils de vente
-                    </p>
-                    <div className='mt-4 '>
-                        <div className=' md:text-2xl text-xl  font-poppinsRegular text-center text-gray-700 '>Votre épanouissement est notre raison d'etre <div className='w-10 h-10 inline-block align-sub '><Emoji4 /></div>  </div>
-                    </div>
-                    <div className=' grid grid-cols-1 md:grid-cols-3 md:px-1  md:gap-16 gap-10  mt-20'>
-                        <BlogCard dataa={data} />
-                        <BlogCard dataa={data} />
-                        <BlogCard dataa={data} />
-                    </div>
-                    <div>
-                        <div className="flex justify-center items-center mt-8 ">
-                            <Button title='Lire plus' />
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div>
+      <div
+        className={` relative bg-lightprimary flex flex-col justify-center items-center  mx-4   sm:max-sm:pb-8 md:mx-24 mt-8 rounded-3xl md:py-20  px-[30px] py-20 mb-16 `}
+      >
+        <div className=" rounded-lg overflow-hidden absolute hidden md:block  top-0 right-0">
+          <Vector4 />
         </div>
-    )
-}
 
-export default Section4
+        <div>
+          <p className=" font-octarine text-center text-2xl md:text-4xl text-black">
+            Nous vous donnons nos conseils de vente
+          </p>
+          <div className="mt-4 ">
+            <div className=" md:text-2xl text-xl  font-poppinsRegular text-center text-gray-700 ">
+              Votre épanouissement est notre raison d&apos;etre{" "}
+              <div className="w-10 h-10 inline-block align-sub ">
+                <Emoji4 />
+              </div>{" "}
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 md:grid-cols-3 md:px-1  md:gap-16 gap-10  mt-20">
+            <BlogCard dataa={data} />
+            <BlogCard dataa={data} />
+            <BlogCard dataa={data} />
+          </div>
+          <div>
+            <div className="flex justify-center items-center mt-8 ">
+              <Button title="Lire plus" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
